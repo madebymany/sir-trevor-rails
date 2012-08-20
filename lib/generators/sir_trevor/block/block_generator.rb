1059b711a369275cc2555f3ebee0e845a2f31a9f
Chris Bell <cjbell1988@gmail.com>
2012-08-20 15:16:19 +0100

    Updated generators to be inline with current version

--- lib/generators/sir_trevor/block/block_generator.rb
+++ lib/generators/sir_trevor/block/block_generator.rb
@@ -16,10 +16,9 @@ module SirTrevor
         # Copy the JS
         copy_file "_block.js", "app/assets/javascripts/sir-trevor/blocks/#{name}.js"
         
-        gsub_file "app/assets/javascripts/sir-trevor/blocks/#{name}.js", /var Name/, "var #{name.capitalize}"
+        gsub_file "app/assets/javascripts/sir-trevor/blocks/#{name}.js", /SirTrevor\.Blocks\.Name/, "SirTrevor.Blocks.#{name.capitalize}"
         gsub_file "app/assets/javascripts/sir-trevor/blocks/#{name}.js", /title: ""/, "title: '#{name.capitalize}'"
         gsub_file "app/assets/javascripts/sir-trevor/blocks/#{name}.js", /className: ""/, "className: '#{name.downcase}'"
-        gsub_file "app/assets/javascripts/sir-trevor/blocks/#{name}.js", /SirTrevor\.BlockTypes\.Name = new Name/, "SirTrevor.BlockTypes.#{name.capitalize} = new #{name.capitalize}"
 
         # Copy the CSS
         copy_file "_block.css.erb", "app/assets/stylesheets/sir-trevor/blocks/#{name}_block.css.erb"
