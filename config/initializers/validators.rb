5f4d696e97a64fdeee967b54c213ee0ebf01070b
Andrew Walker <andy@madebymany.co.uk>
2013-06-11 13:18:17 +0100

    use require to load the active model validator

--- config/initializers/validators.rb
+++ config/initializers/validators.rb
@@ -1,14 +1,12 @@
-include ActiveModel::EachValidator
+require 'active_model/validator'
 
-if defined? ActiveModel::EachValidator
-  class IsJsonValidator < ActiveModel::EachValidator
-    def validate_each(record, attribute, value)
-      @json = JSON.parse(value)
-      record.errors.add(attribute, "is empty") if @json.empty?
-    rescue TypeError => e
-      record.errors.add(attribute, "is not valid JSON: #{e.message}")
-    rescue JSON::JSONError => e
-      record.errors.add(attribute, "is not valid JSON: #{e.message}")
-    end
+class IsJsonValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    @json = JSON.parse(value)
+    record.errors.add(attribute, "is empty") if @json.empty?
+  rescue TypeError => e
+    record.errors.add(attribute, "is not valid JSON: #{e.message}")
+  rescue JSON::JSONError => e
+    record.errors.add(attribute, "is not valid JSON: #{e.message}")
   end
 end
