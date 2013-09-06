a234e94cbf357967794c34499084f34f17a16c9a
Andrew Sprinz <andrewsprinz@gmail.com>
2013-09-06 14:01:40 +0100

    Fixing markdown renderer to handle linebreaks

--- lib/sir-trevor/helpers/view_helper.rb
+++ lib/sir-trevor/helpers/view_helper.rb
@@ -54,9 +54,8 @@ module SirTrevor
       end
 
       def sir_trevor_markdown(text)
-        options = {:hard_wrap => true, :filter_html => true, :autolink => true, :no_intraemphasis => true, :fenced_code => true}
-        renderer = Redcarpet::Render::HTML.new(options)
-        markdown = Redcarpet::Markdown.new(CustomMarkdownFormatter)
+        renderer = CustomMarkdownFormatter.new(:hard_wrap => true, :filter_html => true, :autolink => true, :no_intraemphasis => true, :fenced_code => true)
+        markdown = Redcarpet::Markdown.new(renderer)
         markdown.render(text).html_safe
       end
 
