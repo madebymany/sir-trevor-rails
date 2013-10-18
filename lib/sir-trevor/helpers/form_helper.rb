e30c6b1b71315dc8dc9be9158c2595d5c130edd7
Max McDonnell <max.t.mcdonnell@gmail.com>
2013-10-18 11:52:11 -0400

    fix for rails 4

--- lib/sir-trevor/helpers/form_helper.rb
+++ lib/sir-trevor/helpers/form_helper.rb
@@ -14,16 +14,25 @@ module SirTrevor
         input_html['class'] = "sir-trevor-area visuallyhidden"
         hash = input_html.stringify_keys
 
-        instance_tag = ActionView::Base::InstanceTag.new(object_name, method, self, options.delete(:object))
-        instance_tag.send(:add_default_name_and_id, hash)
-        
+
+        #https://github.com/mjhoy/ckeditor/commit/fca24021e13d355f74c9538f9db27dc58687ce65
+        if defined?(ActionView::Base::InstanceTag)
+          instance_tag = ActionView::Helpers::InstanceTag.new(object_name, method, self, options.delete(:object))
+          instance_tag.send(:add_default_name_and_id, hash)
+          output = instance_tag.to_text_area_tag(input_html)
+        else
+          instance_tag = ActionView::Helpers::Tags::TextArea.new(object_name, method, self, input_html)
+          instance_tag.send(:add_default_name_and_id, hash)
+          output = instance_tag.render
+        end
+
         output_buffer = ActiveSupport::SafeBuffer.new
-        output_buffer << instance_tag.to_text_area_tag(input_html)
-        
+        output_buffer << output
         output_buffer
 
       end
-
     end
   end
 end
+
+         
